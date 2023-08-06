eb00459c1b317d498aad419608bd08dedde0a977
Ezequiel Amin <ezequielamin@outlook.com>
2023-08-06 17:20:52 -0300

    add configs for new endpoint and remove one subject

--- cypress.config.js
+++ cypress.config.js
@@ -14,5 +14,6 @@ module.exports = defineConfig({
         },
       });
     },
+    responseTimeout: 60000,
   },
 });
--- materias.js
+++ materias.js
@@ -1,8 +1,4 @@
-// Set the subjects that should be analized.
+// Set the subjects that should be analyzed.
 export const materias = [
   { id: '411', description: 'Arq. Computadores II' },
-  { id: '412', description: 'Sistemas de Información' },
-  { id: '413', description: 'Ing. Software' },
-  { id: '414', description: 'Teleinformática I' },
-  { id: '415', description: 'Economía' },
 ];
